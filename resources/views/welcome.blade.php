c96a34640c1fda1b5c0a2b9864d0bf1ba51801df
LatrachDev <m.latrach.youcode@gmail.com>
2025-03-25 14:45:10 +0000

    EV-7 design the nav bar for desktop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,18 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 11 with Tailwind</title>
+    <title>EventSphere</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
-<body class="bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text">
+<body class="bg-light-background dark:bg-dark-background dark:text-dark-text">
 
-    <div class="flex justify-between p-4">
-        <h1 class="text-xl font-bold">Laravel Dark Mode</h1>
-        <button onclick="toggleDarkMode()" class="px-4 py-2 border rounded">
-            Toggle Dark Mode 🌙
-        </button>
-    </div>
+    <header class="fixed top-0 w-full flex justify-between items-center px-10 py-5 bg-light-background/50 dark:bg-dark-background/50 backdrop-blur-xl z-50">
+        <img src="{{ asset('images/EventSphere_Logo.png') }}" alt="Logo" class="w-48">
+        <nav class="flex space-x-8 items-center">
+            <a href="" class="text-light-text dark:text-dark-text hover:text-dark-accent dark:hover:text-dark-accent duration-300">Home</a>
+            <a href="" class="text-light-text dark:text-dark-text hover:text-dark-accent dark:hover:text-dark-accent duration-300">Explore</a>
+            <a href="" class="text-light-text dark:text-dark-text hover:text-dark-accent dark:hover:text-dark-accent duration-300">Contact</a>
+            <a href="" class="text-light-text dark:text-dark-text hover:text-dark-accent dark:hover:text-dark-accent duration-300">About</a>
+            
+            <button href="" class="text-light-text dark:text-dark-text hover:text-dark-accent dark:hover:text-dark-accent duration-300" onclick="toggleDarkMode()"><i class="fa-solid fa-sun"></i> Mode</button>
+
+            <a href="" class="text-light-background bg-light-accent py-2 px-6 rounded-full dark:text-dark-text dark:bg-dark-accent hover:bg-light-primary dark:hover:bg-light-primary duration-300">Login</a>
+        </nav>
+    </header>
+
+    
+
 
     <script>
         function toggleDarkMode() {
