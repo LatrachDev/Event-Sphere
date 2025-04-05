df1d6bc6b1965434d1b1788c4390dec6363f60cf
LatrachDev <m.latrach.youcode@gmail.com>
2025-04-05 19:10:26 +0000

    EV-16 authentication routes and middelware for secure routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,23 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// authentification routes
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('guest')->controller(AuthController::class)->group(function (){
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+    Route::get('/index', [AuthController::class, 'index'])->name('index');
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
 });
 
-// authentification 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+Route::middleware('auth')->controller(AuthController::class)->group(function (){
+    Route::get('/home', [AuthController::class, 'home'])->name('home');
+
+});
