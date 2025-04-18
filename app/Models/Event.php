601cd7161766f7d41fac89b3ca2de1ace3f88ad0
LatrachDev <m.latrach.youcode@gmail.com>
2025-04-18 16:29:53 +0100

    EV-33 add models for category and event

---
+++ app/Models/Event.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'start_time',
+        'category_id',
+        'price',
+        'number_of_tickets',
+        'status'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    // public function tickets()
+    // {
+    //     return $this->hasMany(Ticket::class);
+    // }
+}
