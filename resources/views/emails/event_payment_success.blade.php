a53366115c25e1939574bd9131d6a7ce31f4f94b
LatrachDev <m.latrach.youcode@gmail.com>
2025-04-29 16:19:20 +0100

    EV-45 send email to the user with the ticket after payment

---
+++ resources/views/emails/event_payment_success.blade.php
@@ -0,0 +1,12 @@
+<h2>Hi {{ $user->name }},</h2>
+
+<p>Your payment was successful!</p>
+
+<p>You have successfully purchased a ticket for the event:</p>
+<ul>
+    <li><strong>Event Title:</strong> {{ $ticket->event->title }}</li>
+    <li><strong>Date:</strong> {{ $ticket->event->date }}</li>
+    <li><strong>Location:</strong> {{ $ticket->event->location }}</li>
+</ul>
+
+<p>Thank you for using EventSphere. We look forward to seeing you!</p>
